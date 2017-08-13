d837378f0f459606b5b9741745a2550e036d0e59
computer\start <dongsonglangle1990@gmail.com>
2017-08-13 21:30:13 +0700

    update home

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -2,7 +2,10 @@
 App::uses('AppController', 'Controller');
 
 class PagesController extends AppController {
-
+    public function beforeFilter() {
+        parent::beforeFilter();
+        $this->Auth->allow('index');
+    }
 	public function display() {
 		
 	}
