1252f213bb15f151af118dc0f668d1952d98dd35
phamxuancan <dongsonglangle1990@gmail.com>
2017-08-25 17:47:02 +0700

    fsdfs

--- app/Controller/WebhooksController.php
+++ app/Controller/WebhooksController.php
@@ -25,7 +25,34 @@ class WebhooksController extends AppController {
         }
         if($this->request->is('post')){
             $input = json_decode(file_get_contents('php://input'), true);
-            CakeLog::info('requesst : '.json_encode($input));
+            $fanpage_id = isset($input['entry'][0]['id']) ? $input['entry'][0]['id'] : NULL;
+            $sender = isset($input['entry'][0]['messaging'][0]['sender']['id']) ? $input['entry'][0]['messaging'][0]['sender']['id'] : NULL;
+            $message_text = isset($input['entry'][0]['messaging'][0]['message']['text']) ? $input['entry'][0]['messaging'][0]['message']['text'] : NULL;
+            $attachments = isset($input['entry'][0]['messaging'][0]['message']['attachments']) ? $input['entry'][0]['messaging'][0]['message']['attachments'] : NULL;
+            $payload = isset($input['entry'][0]['messaging'][0]['postback']) ? $input['entry'][0]['messaging'][0]['postback'] : NULL;
+            $quick_reply = isset($input['entry'][0]['messaging'][0]['message']['quick_reply']['payload']) ? $input['entry'][0]['messaging'][0]['message']['quick_reply']['payload'] : NULL;
+            if(!empty($fanpage_id)){
+                $this->sendMessageToUser($sender,$message_text);
+            }
         }
+    }
+    private function sendMessageToUser($sender,$message_text){
+        $messageData =array('recipient'=>array('id'=>$sender),'message'=>array('text'=>$message_text));
+        $this->callSendAPI($messageData);
+    }
+    private  function callSendAPI($messageData){
+        try{
+            $url = 'https://graph.facebook.com/v2.10/me/messages';
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            curl_setopt($ch, CURLOPT_POSTFIELDS,
+                $messageData);
+            $head = curl_exec($ch);
+            curl_close($ch);
+        }catch (Exception $e){
+            CakeLog::error('Khong gui dc message');
+        }
+
     }
 }
